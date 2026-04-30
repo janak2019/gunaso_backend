649cab7bcba0ceb037a238cd5ec6e448fff39ec8
htmlbasic <withjanak@gmail.com>
2026-04-30 19:00:12 +0545

    connected to railway mysql

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gunaso",
-});
+dotenv.config();
 
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,   // ✅ important
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+const db = mysql.createConnection(process.env.DATABASE_URL);
 db.connect((err) => {
-  if (err) console.log(err);
-  else console.log("MySQL Connected");
+  if (err) {
+    console.log("❌ DB Error:", err);
+  } else {
+    console.log("✅ Connected to Railway MySQL");
+  }
 });
 
 export default db;
